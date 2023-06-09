dbeb9a98997bbac5d31c251a811fca1fdc47e2ae
alex <ah@gweb.de>
2023-06-09 18:32:44 +0200

    vue + firebase scaffold

---
+++ src/firebase/index.js
@@ -0,0 +1,10 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import firebaseConfig from '@/firebase/config'
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export { db }
